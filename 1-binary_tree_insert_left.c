25b286ed621ad92f14189fe0f6f911ef4041b807
enariyah <hopmichael52@gmail.com>
2023-06-01 23:10:07 +0300

    fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - create a node
+ * binary_tree_insert_left - adds node on the left
  * @parent: pointer to parent
  * @value: value
  * Return: pointer to the created node
